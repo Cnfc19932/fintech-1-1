3cc5224348bee2de35babec6dce1adca887868ca
Stanislav Gorovoy <cnfc19932@gmail.com>
2018-02-25 14:44:43 +0300

    3

--- tasks.js
+++ tasks.js
@@ -1,10 +1,30 @@
 /**
- * Напишите рекурсивную функцию вычисления чисел Фибоначчи
+ * Напишите функцию для вычисления числа Фибоначчи с мемоизацией:
+ * при повторных вызовах функция не вычисляет значения, а достает из кеша.
  * @param {number} x номер числа
  * @return {number} число под номером х
  */
-function fibonacciSimple(x) {
-    if (x === 1) return 1;
-    if (x === 0) return 0;
-    return fibonacciSimple(x-2) + fibonacciSimple(x-1);
+function fibonacciWithCache(x) {
+    //console.log(this.cache);
+    if (this.cache === undefined){
+      this.cache = {};
+    }
+    if (x in this.cache){
+      return this.cache[x];
+    }else{
+      let a;
+      switch(x) {
+        case 1: 
+          a = 1;
+          break;
+        case 0: 
+          a = 0;
+          break;
+        default:
+          a = fibonacciWithCache(x-2) + fibonacciWithCache(x-1);
+          break;
+      }
+      this.cache[x] = a;
+      return a;
+    }
   }
